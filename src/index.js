b2de2afd53abb9e026e6ac6737b7f83b068d7573
tsyn4836 <tsyn4836@gmail.com>
2020-04-11 00:50:25 +0800

    7-3使用styled-components完成Header组件布局

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './style'
+import { GlobalStyle } from './style'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle/>
   </React.StrictMode>,
   document.getElementById('root')
 );
