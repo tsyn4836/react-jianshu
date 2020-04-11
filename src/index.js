860e63e02f5c30b1e8cf535a5aef03952a2d4ccc
tsyn4836 <tsyn4836@gmail.com>
2020-04-12 00:07:00 +0800

    7-11热门搜索样式布局

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { GlobalStyle } from './style'
 import { Iconfont } from './statics/iconfont/iconfont'
 
 ReactDOM.render(
-  <div className="root">
+  <Fragment>
     <App />
     <GlobalStyle />
     <Iconfont />
-  </div>,
+  </Fragment>,
   document.getElementById('root')
 );
